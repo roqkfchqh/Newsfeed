9eba5b62f504ffcf9297e2dc837f7e5740757f47
roqkfchqh <plmokn48@daum.net>
2024-12-20 19:43:56 +0900

    ✨ fix: userRepository merge issue

--- src/main/java/com/example/newsfeed/repository/UserRepository.java
+++ src/main/java/com/example/newsfeed/repository/UserRepository.java
@@ -4,8 +4,12 @@ import com.example.newsfeed.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u.name FROM User u WHERE u.id = :userId")
     String findNameById(Long userId);
+
+    Optional<User> findByEmail(String email);
 }
