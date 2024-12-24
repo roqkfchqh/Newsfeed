ecc522db710f77722feec118a744e70eef897e56
roqkfchqh <plmokn48@daum.net>
2024-12-25 01:50:20 +0900

    fix: postResponseDto 에 id 추가

--- src/main/java/com/example/newsfeed/dto/post/PostResponseDto.java
+++ src/main/java/com/example/newsfeed/dto/post/PostResponseDto.java
@@ -14,6 +14,8 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class PostResponseDto {
 
+    private Long id;
+
     private String title;
 
     private String content;
